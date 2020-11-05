8a4d958d1434fd173881bb92137498b71cf52e17
Shubhang Sharma <shubhang.sharma123@live.com>
2020-11-05 16:08:24 +0530

    blog commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Dev Blog', // Required
+  author: 'Shubhang Sharma', // Required
+  description: 'Software Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://humanoia.github.io/portfolio/',
+    github: 'https://github.com/humanoiA',
+    twitter: 'https://twitter.com/__shubhang__',
+    linkedin: 'https://www.linkedin.com/in/shubhangs/',
+  },
+  pathPrefix: '/blog',
+  siteUrl: 'https://humanoia.github.io/blog/',
+};
